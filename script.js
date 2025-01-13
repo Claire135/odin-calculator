b82a6a3407be2b81928bd8de86bc924d6d7f89e5
Claire O'Brien <186298887+Claire135@users.noreply.github.com>
2025-01-13 18:29:26 +0100

    all in working order apart from errors for decimal points and too many numbers

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, "."]
+const numbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, ".", 0]
 const operands = ["+", "-", "*", "/"]
-const equals = "="
 
 const screenDisplay = document.querySelector("#screenDisplay");
 const numberButtons = document.querySelector("#numberButtons");
+const operandButtons = document.querySelector("#operandButtons");
+const equalsButton = document.querySelector("#equalsButton")
 
 function getNumbers(numbers){
   const buttons = [];
+  const buttons2 = [];
   let num1="";
+  let num2="";
+  let currentOperator = null;
+  let result = null;
 
+  //create and define behaviour for operand buttons
+  for (let j=0; j<operands.length; j++){
+    const opButton = document.createElement("button")
+    opButton.classList.add("opButton")
+    opButton.textContent = operands [j]
+    operandButtons.appendChild(opButton)
+    buttons2.push(opButton)
+
+    opButton.addEventListener("click", function() {
+      currentOperator = operands[j];
+      screenDisplay.textContent = operands[j];
+      console.log("you clicked:" + operands[j])
+    });
+  }
+
+  //create and define behaviour for numbers buttons
   for (let i=0; i<numbers.length; i++){
     
       const indiButton = document.createElement("button")
@@ -18,48 +39,83 @@ function getNumbers(numbers){
       buttons.push(indiButton)
     
       indiButton.addEventListener("click", function() {
-        num1 += numbers[i];
-        screenDisplay.textContent = num1;
-
+        if (currentOperator){ 
+          num2 += numbers[i]
+          screenDisplay.textContent = num2;
+        }else {
+          num1 += numbers[i];
+          screenDisplay.textContent = num1;
+        }
+        
         console.log("you clicked:" + numbers[i])
         console.log(`Num1: ${num1}`);
+        console.log(`Num2: ${num2}`);
       });
   }
-}
 
-getNumbers(numbers);
+  function getResults(){
+    const n1 = parseFloat(num1);
+    const n2= parseFloat(num2);
+    
+    if (n1 > 999999999999999 || n2 > 999999999999999) {
+      screenDisplay.textContent = "Error!";
+    }
+    
+    switch (currentOperator){
+      case("+"):
+        result = n1 + n2;
+        break;
+      
+      case("-"):
+        result = n1 - n2;
+        break;
+      
+      case("*"):
+        result = n1 * n2;
+        break;
+      
+      case("/"):
+        if (n2 === 0){
+          result = "error!"
+        } else {
+        result = n1 / n2
+        }
+        break;
+    }
+    console.log(`Result: ${result}`);
 
-/*
+  }
+
+  //create and define behaviour for equals button
+    const eqButton = document.createElement("button")
+    eqButton.classList.add("eqButton")
+    eqButton.textContent = "="
+    equalsButton.appendChild(eqButton)
+
+    eqButton.addEventListener("click", function() {
+      getResults();
+      screenDisplay.textContent = result
+      num1 = result;
+      num2 = "";
+    }); 
+
   const restart = document.createElement("button");
   restart.classList.add("restart"); 
   restart.textContent = "A/C"; 
-  restartBtn.appendChild(restart);
+  numberButtons.appendChild(restart);
 
-restartBtn.addEventListener("click", (num1, num2, event) => {    
-  document.querySelector("#input").textContent = " ";
-  num1 = 0;
-  num2 = 0;
-});
+  restart.addEventListener("click", function() {    
+    num1 = "";
+    num2 = "";
+    currentOperator = null;
+    result = null;
+    screenDisplay.textContent = "0";
+    console.log(`Num1: ${num1}, Num2: ${num2}, Result: ${result}`)
+  });
 
-*/
-const num1 = [];
-const num2= [];
 
-
-//operand functions:
-
-const add = ((num1, num2) => {
-  return num1 + num2
-});
-
-const subtract = ((num1, num2) => {
-	return num1 - (num2)
-});
-
-const multiply = function(numbers){
-return numbers.reduce((total, curr) => total * curr,1);
 }
 
-const divide = function(numbers){
-  return numbers.reduce((total, curr)=> total / curr,1);
-}
+getNumbers(numbers)
+
+
